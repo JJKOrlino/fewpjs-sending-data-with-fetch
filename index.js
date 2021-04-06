e99645629dd8de3a5ffaba8e0d4a88578c64a709
JJKOrlino <jjkorlino80@yahoo.com>
2021-04-06 13:20:03 -0500

    commit tree dbd065a4175cf5d589f8a900b031a97808ef04db

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+function registerSelf() {
+    return fetch("http://guestbook.example.com/register", {
+      "method": "POST",
+      "body": {
+        "firstName": "Jeremy",
+        "registryMessage": "Registered"
+      }
+    }).then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      return json.message;
+    });
+  }
+  
+  let formData = {
+    "firstName": "Jeremy",
+    "registryMessage": "Look I can do stuff"
+  };
+  
+  let configObj = {
+    method: "POST",
+    body: formData
+  }
+  
+  function errorSelf() {
+    return fetch('http://guestbook.example.com/register-error', configObj)
+    .then(function(res) {
+      return res.json();
+    }).then(function(json) {
+      return json.messsage;
+    }).catch(function(x) {
+      return ":(";
+    })
+  }
